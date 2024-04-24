36a628c42d165be87a54a478118f183b810a2720
KaiduY <alex.oprea2003@gmail.com>
2024-04-24 21:20:14 +0200

    RNG

---
+++ RNG.py
@@ -0,0 +1,41 @@
+import numpy as np
+import secrets
+import matplotlib.pyplot as plt
+from scipy.stats import entropy
+
+N = 42
+BITS_TYPE = [0,1] 
+
+def TRS(N , BITS_TYPE):
+    bits = []
+    while len(bits) < N:
+        bit = secrets.choice(BITS_TYPE)
+        bits.append(bit)
+    return np.array(bits, dtype=np.int16)
+
+def entropy_seq(seq, BITS_TYPE):
+    nbits = len(seq)
+    pk = []
+    for bit in BITS_TYPE:
+        pk.append(np.count_nonzero(seq == bit)/nbits)
+    return entropy(pk, base=len(BITS_TYPE))
+                  
+sample = TRS(N, BITS_TYPE)
+for i in range(1000):
+    return_sample = TRS(N, BITS_TYPE)
+    sample = sample + return_sample
+sample = sample / (i+1.)
+
+fig, ax = plt.subplots()
+ax.stairs(sample, fill = True, color='orange')
+ax.set(xlabel = 'Bit number', ylabel = 'Average bit value', title='Distribution of bit values for each individual bit')
+fig.savefig('TRS_distribution.png')
+
+
+ent = entropy_seq(return_sample, BITS_TYPE)
+
+print(f'Last TRS sample:\n{return_sample}, Entropy: {ent}')
+
+#basis alice [0 1 0 0 0 1 0 1 1 0 0 1 1 1 0 0 0 0 1 1 0 1 1 0 0 1 1 0 0 1 0 0 1 0 0 1 0 1 1 0 1 0], Entropy: 0.9934472383802027
+#bit alice [1 0 1 0 0 0 1 1 1 1 1 1 0 1 1 1 0 0 0 0 1 0 0 1 1 0 0 1 0 1 1 1 1 1 0 1 0 1 1 1 1 0], Entropy: 0.9736680645496202
+#basis bob [1 0 1 1 1 0 0 1 0 1 1 0 0 0 0 1 1 1 0 0 0 0 1 0 1 1 1 1 1 0 1 1 1 1 0 1 1 0 1 1 0 0], Entropy: 0.9852281360342515
